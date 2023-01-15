d02cb3472a05efc085c4994f967c943c7582f42f
themantalope <matthew.antalek@gmail.com>
2023-01-15 16:38:43 -0600

    spelling.....

--- executor/audioclip_text.py
+++ executor/audioclip_text.py
@@ -93,7 +93,7 @@ class AudioCLIPTextEncoder(Executor):
             The accepted keys are ``access_paths`` and ``batch_size`` - in their
             absence their corresponding default values are used.
         """
-        default_logger.debug(f'model loaded, model defice: {self.model.device}')
+        default_logger.debug(f'model loaded, model device: {self.model.device}')
         if not docs:
             return
 
